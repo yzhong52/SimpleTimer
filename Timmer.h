c8b00df3111d45647baf1634f736a71078bbd0ce
Yuchen <yzhong.cs@gmail.com>
2014-03-29 21:51:00 -0400

    Finish Implementation for Windows

--- Timmer.h
+++ Timmer.h
@@ -1,32 +1,32 @@
 #ifndef _TIMMER_H
 #define _TIMEER_H
 
-#include <sstream> 
 #include <string>
+#include <unordered_map>
 
 class Timmer {
 public:
-	static void start( const std::string& clock_name = "" ) {
+	static void begin( const std::string& function_name = "Anonymous Fuction" );
 
-	}
+	static void end( const std::string& function_name = "Anonymous function" ); 
 
-	static void end( const std::string& name ) {
+	static std::string summery( void ); 
 
-	}
-
-	static std::string summery( void ) {
-		std::stringstream ss; 
-		ss << "+---------------+--------------+---------------+----------------------+" << std::endl; 
-		ss << "| Function Name | Running Time | Excuted Times | Everage Running Time |" << std::endl; 
-		ss << "+---------------+--------------+---------------+----------------------+" << std::endl; 
-		return ss.str();
-	}
 private:
-	Timmer() { }
-	Timmer& Instance() { 
-		static Timmer timmer; 
-		return timmer; 
+	// Private Functions
+	Timmer();
+	inline static Timmer& instance(){
+		static Timmer t; 
+		return t; 
 	}
+	// Data Structures
+	struct Data{
+		double total_run_time; 
+		double begin_time; 
+		int count; 
+		Data() : total_run_time( 0.0 ), begin_time( 0.0 ), count(0) { } 
+	}; 
+	std::unordered_map<std::string, Data> datas; 
 }; 
 
 #endif 
--- example.cpp
+++ example.cpp
@@ -1,10 +1,39 @@
 #include "Timmer.h"
 #include <iostream>
+#include <math.h>
 
 using namespace std; 
 
-int main(void) {
-	std::cout << " Hello Clock " << std::endl; 
+#include <ctime>
+
+
+float function1( int i ) {
+	return tan( 1.0f * i ); 
+}
+
+float function2( int i ) {
+	return cos( 1.0f * i ) * sin( 1.0f * i );
+}
+
+
+#include <windows.h>
+int main()
+{
+	for( int i=0; i<1000; i++ ) {
+		Timmer::begin( "Function A" ); 
+		function1(i);
+		Timmer::end( "Function A" ); 
+
+		Timmer::begin( "Function B" ); 
+		function2(i);
+		Timmer::end( "Function B" ); 
+	}
+	
+	for( int i=0; i<20; i++ ) {
+		Timmer::begin( "Function B" ); 
+		function2(i);
+		Timmer::end( "Function B" ); 
+	}
 
 	cout << Timmer::summery() << endl; 
 
