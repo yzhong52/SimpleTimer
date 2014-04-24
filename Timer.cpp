fdebf5981d58fb00c7800de5f811c0f71aa89e25
Yuchen <yzhong.cs@gmail.com>
2014-04-24 17:34:42 -0400

    also output the total running time

--- Timer.cpp
+++ Timer.cpp
@@ -49,23 +49,23 @@ Timer::Timer(){
 	whole_program.count = 1; 
 }
 
-void Timer::begin( const std::string& function_name ) {
-	std::unordered_map<std::string, Data>::iterator it = instance().datas.find( function_name ); 
+void Timer::begin( const std::string& func_name ) {
+	std::unordered_map<std::string, Data>::iterator it = instance().datas.find( func_name ); 
 	if( it==instance().datas.end() ) {
-		instance().datas.insert( pair<std::string, Data>( function_name, Data()) ); 
-		it = instance().datas.find( function_name ); 
+		instance().datas.insert( pair<std::string, Data>( func_name, Data()) ); 
+		it = instance().datas.find( func_name ); 
 	}
 	it->second.begin_time = get_current_time(); 
 }
 
 
-void Timer::end( const std::string& function_name ) {
-	std::unordered_map<std::string, Data>::iterator it = instance().datas.find( function_name ); 
+void Timer::end( const std::string& func_name ) {
+	std::unordered_map<std::string, Data>::iterator it = instance().datas.find( func_name ); 
 	if( it!=instance().datas.end() ) {
 		it->second.total_run_time += get_current_time() - it->second.begin_time; 
 		it->second.count++; 
 	} else {
-		std::cerr << "Error: This function '" << function_name << "' is not defined" << std::endl; 
+		std::cerr << "Error: This function '" << func_name << "' is not defined" << std::endl; 
 		system( "pause" ); 
 	}
 }
@@ -79,13 +79,13 @@ std::string Timer::summery( void ) {
 	static const int func_name_size    = max((int)func_name.length(),    22);
 	
 	static const string total_time   = "Total"; 
-	static const int total_time_size   = max((int)total_time.length(),   15);
+	static const int total_time_size   = max((int)total_time.length(),   22);
 	
 	static const string called_times = "Be Called"; 
 	const int called_times_size = max((int)called_times.length(), 12);
 	
 	static const string percentage   = "Percentage"; 
-	const int percentage_size   = max((int)percentage.length(),   15);
+	const int percentage_size   = max((int)percentage.length(),   12);
 
 	ss << "+--------------------" << endl;
 	ss << "| Profiling Summery ..." << endl;
@@ -104,9 +104,20 @@ std::string Timer::summery( void ) {
 	ss << std::right << percentage;
 	ss << " |" << std::endl; 
 	
-	std::unordered_map<std::string, Data>::iterator it; 
+	 
 	double tatol_running_time = Timer::instance().whole_program.total_run_time;
 
+	ss << "| "; 
+	ss.width( func_name_size );
+	ss << std::left << "Total Run Time" << " |"; 
+	ss.width( total_time_size - sizeof("ms") );
+	ss << std::right << tatol_running_time << " ms |"; 
+	ss.width( called_times_size - sizeof("times") );
+	ss << std::right << 1 << " times |"; 
+	ss.width( percentage_size - sizeof("%") );
+	ss << std::right << 100.000 << " % |" << endl; 
+
+	std::unordered_map<std::string, Data>::iterator it;
 	for( it = instance().datas.begin(); it != instance().datas.end(); it++ ) {
 		ss << "| "; 
 		ss.width( func_name_size );
