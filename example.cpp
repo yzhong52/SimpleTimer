7806a1a3f4d0968910b55bb183224a8d341f04d3
Yuchen <yzhong.cs@gmail.com>
2014-04-26 10:32:26 -0400

    update example

--- example.cpp
+++ example.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 #include <math.h>
 #include <algorithm>
-using namespace std; 
+using namespace std;
 
+// computing PI
+// reference: http://mathworld.wolfram.com/PiFormulas.html
 double func_PI( int n ) {
 	Timer::begin( "Compute Pi" ); 
 
-	// computing pi
-	// reference: http://mathworld.wolfram.com/PiFormulas.html
-	double res = 0; 
+	double res = 0;
 	for( int k=1; k<n; k++ ) {
 		res += 4 * pow(-1.0, k+1) / ( 2 * k - 1 ); 
 	}
@@ -18,15 +18,14 @@ double func_PI( int n ) {
 	return res;
 }
 
+// computing the fibonaci number
 int func_Fibonacci( int n ) {
 	Timer::begin( "Function B" ); 
 	
-	// computing the fibonaci
-	int res = 1; 
-	int tmp = 1; 
+	int res = 1, tmp = 1;
 	for( int i=0; i<n; i++ ) {
 		tmp = res + tmp; 
-		std::swap( res, tmp ); 
+		swap( res, tmp );
 	}
 	
 	Timer::end( "Function B" ); 
@@ -36,8 +35,8 @@ int func_Fibonacci( int n ) {
 
 int main()
 {
-	cout << "Pi: " << func_PI( 1000000 ) << endl;
-	cout << "Fibonacci i: " << func_Fibonacci( 1000000 ) << endl;;
+    func_PI( 1000000 );
+    func_Fibonacci( 1000000 );
 	
 	cout << Timer::summery() << endl; 
 
